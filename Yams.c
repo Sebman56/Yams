bce299e22b41ab62e48813d71b3cefaa4676a286
Seb <113142393+Sebman56@users.noreply.github.com>
2025-01-11 03:10:13 +0100

    Update Yams.c
    Penser à changer char en bool dans Tour

--- Yams.c
+++ Yams.c
@@ -1,112 +1,465 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-//Compiler version gcc  6.3.0
+int i;
+int n;
+int A;
+int B;
+int C;
+int D;
+int E;
+int N;
+int NoJoueur;
+int NbreD;
+int Etat [][5];
 
-int n, A, B, C, D, E;
+int ReponseA;
+int ReponseB;
+int ReponseC;
+int ReponseD;
+int ReponseE;
+int NPlayer;
 
-int Random ()
-{  
-  n = rand()  % 6+ 1;
-  return n;
-}
+int egaliteA;
+int egaliteB;
+int egaliteC;
+int egaliteD;
+int egaliteE;
+
+int Compteur;
+int CompteurPetiteSuite;
+int CompteurGrandeSuite;
+int Somme;
+
+/************************************************************************ */
+/* ****************************************** D claration des fonctions */
+/************************************************************************* */
+ int NombreDeJoueur(void);
+ int Tirage(void);
+ int SaisieDes(void);
+ int Random (void);
+
+ int Egalite(void);
+ int GrandeSuite (void);
+ int PetiteSuite (void);
+ int ConclusionEgalite (void);
+ int CalculMoins8Plus25 (void);
+
+ int RemplissageTableauEtat(); ///
+ int Tour1();
+ int Lecture ();
+
+/************************************************************************ */
+/* ****************************************** Developpement des fonctions */
+/************************************************************************ */
 
 
-int Tirage()
+/************************************************************************ */
+/* ***************** Developpement des fonctions des gestions des joueurs */
+/************************************************************************ */
+int main()
 {
-  srand(time(NULL));
-  
-  Random();
-  A = n;
-  printf("Dé numéro A - %i\n",A);
-  
-  Random();
-  B = n;
-  printf("Dé numéro B - %i\n",B);
- 
-  Random();
-  C = n;
-  printf("Dé numéro C - %i\n",C);
- 
-  Random();
-  D = n;
-  printf("Dé numéro D - %i\n",D);
-  
-  Random();
-  E = n;
-  printf("Dé numéro E - %i\n",E);
- 
-  printf ("\n");
-  return 0;
+    NombreDeJoueur();
+    printf ("\n Nombre de joueur:  %d ", NPlayer);
+
+    Tirage ();
+    Tour1();
+    printf ("\n\tLes valeurs des 05 D s sont: %i, %i, %i, %i, %i\n", A, B, C, D, E);
+
+    Lecture ();
+    printf ("\n\tLes valeurs des 05 D s sont: %i, %i, %i, %i, %i\n", A, B, C, D, E);
+
+    printf(" \n\n\n");
+    return 0;
+    exit(0);
 }
+
+
+int NombreDeJoueur()
+{
+   printf("\n\t Entrez le nombre de joueur (Max 6): ");
+   scanf("%i", &NPlayer);
+
+   if (NPlayer < 1 || NPlayer > 6)
+   {
+      printf("\n\t Nombre de joueur erron , au revoir.");
+      return 0;
+      exit(0);
+   }
+}
+
+
+
+int Tour1()
+{
+//   int Etat[NPlayer][5];
+   for (NbreD = 1; NbreD < 6; NbreD++) // Lanc  de 5 d s
+   {
+      Random();
+      Etat[NoJoueur][NbreD] = n;
+   }
+    A = Etat[NoJoueur][1];
+    B = Etat[NoJoueur][2];
+    C = Etat[NoJoueur][3];
+    D = Etat[NoJoueur][4];
+    E = Etat[NoJoueur][5];
+   printf("\n");
+   //   printf ("\n\t Tirage Joueur %i: %i",NPlayer, n);
+   printf(" \n Tour Joueur %i: %i, %i, %i, %i, %i", NoJoueur, Etat[NoJoueur][1], Etat[NoJoueur][2], Etat[NoJoueur][3], Etat[NoJoueur][4], Etat[NoJoueur][5]);
+   // Lecture ();
+}
+
+int Tour2et3()
+{
+}/*
+    int SelectionDe ()
+    {
+        printf ("\n Voulez_vous relancer le D  \"A\" qui vaut %i ?  (oui=O  ; Non =N)", A);
+            scanf("%c", &ReponseA);
+        printf ("\n Voulez_vous relancer le D  \"B\" qui vaut %i ?  (oui=O  ; Non =N)", B);
+            scanf("%c", &ReponseB);
+        printf ("\n Voulez_vous relancer le D  \"C\" qui vaut %i ?  (oui=O  ; Non =N)", C);
+            scanf("%c", &ReponseC);
+        printf ("\n Voulez_vous relancer le D  \"D\" qui vaut %i ?  (oui=O  ; Non =N)", D);
+            scanf("%c", &ReponseD);
+        printf ("\n Voulez_vous relancer le D  \"E\" qui vaut %i ?  (oui=O  ; Non =N)", E);
+            scanf("%c", &ReponseE);
+   
+           if (ReponseA = N){
+            Random();
+            A = n ;
+            Etat[NoJoueur][1]=A;}
+
+        if (ReponseB = N){
+            Random();
+            B = n;
+            Etat[NoJoueur][1]=B;}
+
+        if (ReponseC = N){
+            Random();
+            C = n;
+            Etat[NoJoueur][3]=C;}
+
+        if (ReponseD = N){
+            Random();
+            D = n;
+            Etat[NoJoueur][4]=D;}
+       
+        if (ReponseE = N){
+            Random();
+            E = n;
+            Etat[NoJoueur][5]= E;}
+   
+    return 0;
+}*/
+
+
+
+
+/* ********************** Remplissage total et automatique du tableau Etat
+
+   int Etat[NPlayer][5];   // construction du tableau Etat
+   int i, j;
+   for (i=0; i<NPlayer; i++)
+      {
+      printf ("\n");
+      for (j=0; j<5; j++)
+         {
+         Random();
+         Etat[i][j] = n;
+         //Etat[0][j] = i;
+         printf ("\n\t Tirage Joueur %i: %i",i, n);
+         }
+      }
+      printf ("\n");
+      printf("\n================================");
+      // printf ("\n\t Tirage Joueur %i: %i\n",i-1, n-4);
+      printf ("\n\n");
+*/
+
+/* ***************************************  Affichage Etat
+      for (i=0 ; i<NPlayer ; i++)
+         {
+         for (j= 0; j<5 ; j++)
+            {
+            printf (" \n Joueur %i: %i, %i, %i, %i, %i", i+1, Etat[i][j], Etat[i][j], Etat[i][j], Etat[i][j], Etat[j][j]);
+            }
+         printf ("\n");
+         }     //  printf (" Joueur %i: %i, %i, %i, %i, " Etat [i][])
+*/
+/*
+int RemplissageTableauEtat()
+{
+   //int Etat[NPlayer][5];                              // construction du tableau Etat
+   for (NoJoueur = 0; NoJoueur < NPlayer; NoJoueur++) //
+   {
+      Tour();
+   }
+   // printf(" \n RemplissageTableauEtat Joueur %i: %i, %i, %i, %i, %i", NoJoueur, Etat[NoJoueur][0], Etat[NoJoueur][1], Etat[NoJoueur][2], Etat[NoJoueur][3], Etat[NoJoueur][4]);
+}
+*/
+
+
+/************************************************************************ */
+/* ********** Developpement des fonctions de Lecture v rification des d s */
+/************************************************************************ */
+
+int Lecture ()
+{
+     CalculMoins8Plus25();
+     PetiteSuite();
+     GrandeSuite();
+     Egalite();
+     ConclusionEgalite();
+     return 0;
+}
+
+int CalculMoins8Plus25 ()
+{
+     Somme = A+ B + C + D + E;
+     printf ("\n\t Calcul du moins 8 et plus 25.\n");
+     if ((Somme) < 8)
+     {printf ("\t\t *** Moins 8 d tect .\n");}
+
+     if ((Somme) > 25)
+     {printf ("\t\t *** Plus 25 d tect .\n");}
+
+     if ((Somme >= 8) && (Somme <= 25))
+     {printf ("\t\t  *** Pas de moins 8 ou de plus 25 d tect . --- %i --\n",Somme);}
+}
+
+int PetiteSuite ()
+{
+   printf ("\n\t Calcul de Petite suite.\n");        
+   if
+   (
+   ((A == 1) || (B== 1) || (C== 1) || (D== 1) || (E== 1))
+   && ((A == 2) || (B == 2) || (C == 2) || (D == 2) || (E == 2))
+   && ((A == 3) || (B == 3) || (C == 3) || (D == 3) || (E == 3))
+   && ((A == 4) || (B == 4) || (C == 4) || (D == 4) || (E == 4))
+   )
+     {
+     printf("\t\t *** Suite 1234 detect e.\n");
+     //printf("\t rappel valeur: %i, %i, %i, %i, %i",A,B,C,D,E);
+     CompteurPetiteSuite++;
+        }
+
+   if
+   (
+    ((A == 5) || (B == 5) || (C == 5) || (D == 5) || (E == 5))
+   && ((A == 2) || (B == 2) || (C == 2) || (D == 2) || (E == 2))
+   && ((A == 3) || (B == 3) || (C == 3) || (D == 3) || (E == 3))
+   && ((A == 4) || (B == 4) || (C == 4) || (D == 4) || (E == 4))
+   )
+        {printf("\t\t *** Suite 2345 detect e.\n");
+        CompteurPetiteSuite++;}
+
+   if
+   (
+    ((A == 6) || (B == 6) || (C == 6) || (D == 6) || (E == 6))
+   && ((A == 5) || (B == 5) || (C == 5) || (D == 5) || (E == 5))
+   && ((A == 3) || (B == 3) || (C == 3) || (D == 3) || (E == 3))
+   && ((A == 4) || (B == 4) || (C == 4) || (D == 4) || (E == 4))
+   )
+        {printf("\t\t *** Suite 3456 detect e.\n");
+        CompteurPetiteSuite++;}
+
+   if (CompteurPetiteSuite == 0)
+     {printf ("\t\t *** Pas de Petite suite d tect e !!! \n");}
+     //printf ("\n");
+}
+
+
+int GrandeSuite ()
+{
+   printf ("\n\t Calcul de Grande suite.\n");
+
+        /* Verification de la somme de 4 des 5 D s *//*1234 = 10*/
+
+   if
+   (
+   ((A == 1) || (B== 1) || (C== 1) || (D== 1) || (E== 1))
+   && ((A == 2) || (B == 2) || (C == 2) || (D == 2) || (E == 2))
+   && ((A == 3) || (B == 3) || (C == 3) || (D == 3) || (E == 3))
+   && ((A == 4) || (B == 4) || (C == 4) || (D == 4) || (E == 4))
+   && ((A == 5) || (B == 5) || (C == 5) || (D == 5) || (E == 5))
+   )
+     {
+     printf("\t\t *** Grande Suite 12345 detect e.\n");
+     //printf("\t rappel valeur: %i, %i, %i, %i, %i",A,B,C,D,E);
+     CompteurGrandeSuite++;
+        }
+
+   if
+   (
+    ((A == 5) || (B == 5) || (C == 5) || (D == 5) || (E == 5))
+   && ((A == 2) || (B == 2) || (C == 2) || (D == 2) || (E == 2))
+   && ((A == 3) || (B == 3) || (C == 3) || (D == 3) || (E == 3))
+   && ((A == 4) || (B == 4) || (C == 4) || (D == 4) || (E == 4))
+   && ((A == 6) || (B == 6) || (C == 6) || (D == 6) || (E == 6))
+   )
+        {printf("\t\t *** Grande Suite 23456 detect e.\n");
+        CompteurGrandeSuite++;}
+
+   if (CompteurGrandeSuite == 0)
+     {printf ("\t\t *** Pas de Grande suite d tect e !!! \n");}
+     //printf ("\n");
+}
+
 
 int Egalite()
 {
-  int egaliteA = 0;
-  if (A == B) {egaliteA = egaliteA +1; printf ("egalitéA: %i\n",egaliteA);}
-  if (A == C) {egaliteA = egaliteA +1; printf ("egalitéA: %i\n",egaliteA);}
-  if (A == D) {egaliteA = egaliteA +1; printf ("egalitéA: %i\n",egaliteA);}
-  if (A == E) {egaliteA = egaliteA +1; printf ("egalitéA: %i\n",egaliteA);}
-    if (egaliteA == 1) { printf ("\n\t Il y a une paire avec le Dé A");}
-    if (egaliteA == 2) { printf ("\n\t Il y a un brelan  avec le Dé A");}
-    if (egaliteA == 3) { printf ("\n\t Il y a un carré avec le Dé A");}
-        printf ("\n");
+   printf ("\n\t D tection de D s identiques.");
+   egaliteA = 0;
+   if (A == B) {egaliteA = egaliteA +1;}
+   if (A == C) {egaliteA = egaliteA +1;}
+   if (A == D) {egaliteA = egaliteA +1;}
+   if (A == E) {egaliteA = egaliteA +1;}
+     if (egaliteA == 1) { printf ("\n\t\t Il y a une paire avec le D  A");}
+     if (egaliteA == 2) { printf ("\n\t\t Il y a un brelan  avec le D  A");}
+     if (egaliteA == 3) { printf ("\n\t\t Il y a un carr  avec le D  A");}
 
-  int egaliteB = 0;
-  if (B == A) {egaliteB = egaliteB +1; printf ("egalitéB: %i\n",egaliteB);}
-  if (B == C) {egaliteB = egaliteB +1; printf ("egalitéB: %i\n",egaliteB);}
-  if (B == D) {egaliteB = egaliteB +1; printf ("egalitéB: %i\n",egaliteB);}
-  if (B == E) {egaliteB = egaliteB +1; printf ("egalitéB: %i\n",egaliteB);}
-    if (egaliteB == 1) { printf ("\n\t Il y a une paire avec le Dé B");}
-    if (egaliteB == 2) { printf ("\n\t Il y a un brelan  avec le Dé B");}
-    if (egaliteB == 3) { printf ("\n\t Il y a un carré avec le Dé B");}
-      printf ("\n");
+   egaliteB = 0;
+   if (B == A) {egaliteB = egaliteB +1;}
+   if (B == C) {egaliteB = egaliteB +1;}
+   if (B == D) {egaliteB = egaliteB +1;}
+   if (B == E) {egaliteB = egaliteB +1;}
+     if (egaliteB == 1) { printf ("\n\t\t Il y a une paire avec le D  B");}
+     if (egaliteB == 2) { printf ("\n\t\t Il y a un brelan  avec le D  B");}
+     if (egaliteB == 3) { printf ("\n\t\t Il y a un carr  avec le D  B");}
 
-  int egaliteC = 0;
-  if (C == A) {egaliteC = egaliteC +1; printf ("egalitéC: %i\n",egaliteC);}
-  if (C == B) {egaliteC = egaliteC +1; printf ("egalitéC: %i\n",egaliteC);}
-  if (C == D) {egaliteC = egaliteC +1; printf ("egalitéC: %i\n",egaliteC);}
-  if (C == E) {egaliteC = egaliteC +1; printf ("egalitéC: %i\n",egaliteC);}
-    if (egaliteC == 1) { printf ("\n\t Il y a une paire avec le Dé C");}
-    if (egaliteC == 2) { printf ("\n\t Il y a un brelan  avec le Dé C");}
-    if (egaliteC == 3) { printf ("\n\t Il y a un carré avec le Dé C");}
-        printf ("\n");
+   egaliteC = 0;
+   if (C == A) {egaliteC = egaliteC +1;}
+   if (C == B) {egaliteC = egaliteC +1;}
+   if (C == D) {egaliteC = egaliteC +1;}
+   if (C == E) {egaliteC = egaliteC +1;}
+     if (egaliteC == 1) { printf ("\n\t\t Il y a une paire avec le D  C");}
+     if (egaliteC == 2) { printf ("\n\t\t Il y a un brelan  avec le D  C");}
+     if (egaliteC == 3) { printf ("\n\t\t Il y a un carr  avec le D  C");}
 
-  int egaliteD = 0;
-  if (D == A) {egaliteD = egaliteD +1; printf ("egalitéD: %i\n",egaliteD);}
-  if (D == B) {egaliteD = egaliteD +1; printf ("egalitéD: %i\n",egaliteD);}
-  if (D == C) {egaliteD = egaliteD +1; printf ("egalitéD: %i\n",egaliteD);}
-  if (D == E) {egaliteD = egaliteD +1; printf ("egalitéD: %i\n",egaliteD);}
-    if (egaliteD == 1) { printf ("\n\t Il y a une paire avec le Dé D");}
-    if (egaliteD == 2) { printf ("\n\t Il y a un brelan  avec le Dé D");}
-    if (egaliteD == 3) { printf ("\n\t Il y a un carré avec le Dé D");}
-        printf ("\n");
+   egaliteD = 0;
+   if (D == A) {egaliteD = egaliteD +1;}
+   if (D == B) {egaliteD = egaliteD +1;}
+   if (D == C) {egaliteD = egaliteD +1;}
+   if (D == E) {egaliteD = egaliteD +1;}
+     if (egaliteD == 1) { printf ("\n\t\t Il y a une paire avec le D  D");}
+     if (egaliteD == 2) { printf ("\n\t\t Il y a un brelan  avec le D  D");}
+     if (egaliteD == 3) { printf ("\n\t\t Il y a un carr  avec le D  D");}
 
-  int egaliteE = 0;
-  if (E == A) {egaliteE = egaliteE +1; printf ("egalitéE: %i\n",egaliteE);}
-  if (E == B) {egaliteE = egaliteE +1; printf ("egalitéE: %i\n",egaliteE);}
-  if (E == C) {egaliteE = egaliteE +1; printf ("egalitéE: %i\n",egaliteE);}
-  if (D == E) {egaliteE = egaliteE +1; printf ("egalitéE: %i\n",egaliteE);}
-    if (egaliteE == 1) { printf ("\n\t Il y a une paire avec le Dé E");}
-    if (egaliteE == 2) { printf ("\n\t Il y a un brelan  avec le Dé E");}
-    if (egaliteE == 3) { printf ("\n\t Il y a un carré avec le Dé E");}
-        printf ("\n");
+   egaliteE = 0;
+   if (E == A) {egaliteE = egaliteE +1;}
+   if (E == B) {egaliteE = egaliteE +1;}
+   if (E == C) {egaliteE = egaliteE +1;}
+   if (D == E) {egaliteE = egaliteE +1;}
+     if (egaliteE == 1) { printf ("\n\t\t Il y a une paire avec le D  E");}
+     if (egaliteE == 2) { printf ("\n\t\t Il y a un brelan  avec le D  E");}
+     if (egaliteE == 3) { printf ("\n\t\t Il y a un carr  avec le D  E");}
 
-  // if (egalite > 0) {egalite = egalite +1;}
-  
-  // egalite = egalite /2;
-
-  // printf ("\n");
-  printf ("\n\t Nombre de Dé(s) identique(s) à A: %i", egaliteA);
-  printf ("\n\t Nombre de Dé(s) identique(s) à B: %i", egaliteB);
-  printf ("\n\t Nombre de Dé(s) identique(s) à C: %i", egaliteC);
-  printf ("\n\t Nombre de Dé(s) identique(s) à D: %i", egaliteD);
-  printf ("\n\t Nombre de Dé(s) identique(s) à E: %i\n", egaliteE);
-  printf ("\n");
+   /* *** Detection du Yams */
+   if ((A == B) && (B == C) && (C == D) && (D ==E))
+   { printf ("\n\t\t  !!! Yams d tect . !!!");}
+   printf ("\n");
 }
 
-int main()
+int ConclusionEgalite()
 {
-  Tirage();
-  Egalite();
+   // printf ("\n");
+   printf ("\n\t Nombre de D (s) identique(s)   A: %i", egaliteA);
+   printf ("\n\t Nombre de D (s) identique(s)   B: %i", egaliteB);
+   printf ("\n\t Nombre de D (s) identique(s)   C: %i", egaliteC);
+   printf ("\n\t Nombre de D (s) identique(s)   D: %i", egaliteD);
+   printf ("\n\t Nombre de D (s) identique(s)   E: %i\n", egaliteE);
+   printf ("\n");
+}
+
+/************************************************************************ */
+int Random()
+{
+   n = rand() % 6 + 1;
+   return n;
+}
+
+
+
+
+/************************************************************************* */
+/* ****************************************** Developpement de la fonction Tirage ()*/
+/************************************************************************* */
+
+/*
+int Tirage() // *** Fonction Tirage par saisie de l'utilisateur
+{
+   printf ("\n");
+
+   printf("Entrez la valeur du D  num ro A: ");
+   scanf ("%i",&A);
+   printf("Entrez la valeur du D  num ro B: ");
+   scanf ("%i",&B);
+   printf("Entrez la valeur du D  num ro C: ");
+   scanf ("%i",&C);
+   printf("Entrez la valeur du D  num ro D: ");
+   scanf ("%i",&D);
+   printf("Entrez la valeur du D  num ro E: ");
+   scanf ("%i",&E);
+
+   printf ("\n\tLes valeurs des 05 D s sont: %i, %i, %i, %i, %i\n", A,
+B, C, D, E);
+   return 0;
+}
+*/
+
+int Tirage() // *** Fonction Tirage avec Random
+{
+   srand(time(NULL));
+   printf("\n");
+   Random();
+   A = n;
+   printf("D  num ro A - %i\n", A);
+
+   Random();
+   B = n;
+   printf("D  num ro B - %i\n", B);
+
+   Random();
+   C = n;
+   printf("D  num ro C - %i\n", C);
+
+   Random();
+   D = n;
+   printf("D  num ro D - %i\n", D);
+
+   Random();
+   E = n;
+   printf("D  num ro E - %i\n", E);
+
+   // TableauScore[5] = { A , B , C , D , E}; //1 , 2 , 3 , 56, 76 };
+
+   printf("\n");
+   return 0;
+}
+
+/*
+int Tirage() // *** Fonction Tirage avec des variables fixe
+{
+  printf ("\n");
+
+  A = 1;
+  printf("D  num ro A - %i\n",A);
+
+  B = 1;
+  printf("D  num ro B - %i\n",B);
+
+  C = 2;
+  printf("D  num ro C - %i\n",C);
+
+  D = 3;
+  printf("D  num ro D - %i\n",D);
+
+  E = 4;
+  printf("D  num ro E - %i\n",E);
+
+  printf ("\n");
   return 0;
 }
+*/
